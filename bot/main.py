e4d99cd35c1bcfb6bacdd9d8d1b0a73e973cd869
Ramil Shafikov <rshafikov.work@yandex.ru>
2024-04-29 16:43:26 +0200

    initial commit

--- bot/main.py
+++
@@ -1,8 +0,0 @@
-# from test_bot import TestBot
-from variables import TOKEN
-
-from bot import MyBot
-
-if __name__ == '__main__':
-    bot = MyBot(TOKEN)
-    bot.start_polling()
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="plus_balance",
-    setup_requires=['pbr'],
-    python_requires='>=3.9',
-    pbr=True,
-)
